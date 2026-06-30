778f4f3f41c63a2291fe04290217debda9998006
Abhi <johnabhi744@gmail.com>
2026-07-01 02:01:36 +0530

    Extracted text from pdf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
+from src.load_and_extract_text import extract_text_from_pdf
 
 from dotenv import load_dotenv
 
@@ -11,4 +12,9 @@ embedding_model=os.getenv("EMBEDDING_MODEL")
 llm_model=os.getenv("LLM_MODEL")
 
 llm=ChatGroq(groq_api_key=groq_api_key,model_name=llm_model)
-print(llm.invoke("who is Hardik Pandya?"))
+# print(llm.invoke("who is Hardik Pandya?"))
+
+if __name__=="__main__":
+    extracted_text=extract_text_from_pdf("research-paper.pdf")
+    print(extracted_text)
+    
